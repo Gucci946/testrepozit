b043e05c7de3cbdefea8ce93ceb2ee38dafe9785
AntonchikGucci <guccianton777@gmail.com>
2025-10-16 10:48:33 +0300

    switchrandomnr

---
+++ SwitchRandomNumbers/SwitchRandomNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace SwitchRandomNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //genereerib iha kordnsuvalised numbrid
+            //1-st kuni 6-ni
+
+            //peatebkasutama switchi, et igat numbrit näidata
+        }
+    }
+}
+
+
+
+
+
