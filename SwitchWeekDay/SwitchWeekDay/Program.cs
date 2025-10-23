0d036cf161d8122132fd5ade6c71b876feea4937
Opilane TTHK <opilane@tthk.ee>
2025-10-23 11:40:06 +0300

    switchdaynr

---
+++ SwitchWeekDay/SwitchWeekDay/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace SwitchWeekDay
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            //tuleb kasutada switchi
+            //kui on esmaspäev, siis konsoolis kuvatakse
+            //"Esmspäev on tööpäev"
+            //peate kasutama DayOfWeek enumertrsiooni
+        {
+            // Võtame tänase päeva süsteemist
+            DayOfWeek today = DateTime.Now.DayOfWeek;
+
+            // Kuvame tänase päeva nime
+            Console.WriteLine($"Täna on {today}.");
+
+            // Kasutame switch-lause abil kontrolli
+            switch (today)
+            {
+                case DayOfWeek.Monday:
+                    Console.WriteLine("Esmaspäev on tööpäev.");
+                    break;
+
+                case DayOfWeek.Tuesday:
+                    Console.WriteLine("Teisipäev on tööpäev.");
+                    break;
+
+                case DayOfWeek.Wednesday:
+                    Console.WriteLine("Kolmapäev on tööpäev.");
+                    break;
+
+                case DayOfWeek.Thursday:
+                    Console.WriteLine("Neljapäev on tööpäev.");
+                    break;
+
+                case DayOfWeek.Friday:
+                    Console.WriteLine("Reede on tööpäev.");
+                    break;
+
+                case DayOfWeek.Saturday:
+                    Console.WriteLine("Laupäev on nädalavahetus!");
+                    break;
+
+                case DayOfWeek.Sunday:
+                    Console.WriteLine("Pühapäev on nädalavahetus!");
+                    break;
+
+                default:
+                    Console.WriteLine("Midagi läks valesti!");
+                    break;
+            }
+
+            Console.ReadLine(); // et aken kohe ei sulguks
+        }
+    }
+}
