a87a7f19c257e1d8b4829e174ee320f8ea532315
Gucci946 <antonchikgucci@gmail.com>
2025-11-20 10:36:27 +0200

    While Do

---
+++ SwitchTryCatch/SwitchTryCatch/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace SwitchTryCatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Try Catch koos Switch-ga");
+            Console.WriteLine("Sisesta number 1 või 2");
+
+            try
+            {
+                int input = int.Parse(Console.ReadLine());
+
+                switch (input)
+                {
+                    case 1:
+                        Console.WriteLine("Sisestasid numbri 1");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Sisestasid numbri 2");
+                        break;
+
+                    default:
+                        Console.WriteLine("Vali ainult 1 või 2!");
+                        break;
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Palun sisesta kehtiv number.");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Sisestatud number on liiga suur või liiga väike.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Tekkis ootamatu viga: {ex.Message}");
+            }
+        }
+    }
+}
+
