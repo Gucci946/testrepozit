2b0b9a848bd3f13b6bae1b48229f12810f108b25
Gucci946 <antonchikgucci@gmail.com>
2025-11-20 11:41:54 +0200

    While do 2 triangle

---
+++ TriangleWhileDo/TriangleWhileDo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Kolmnurk:");
+        int n = int.Parse(Console.ReadLine());
+
+        int i = 1;                
+
+        while (i <= n)
+        {
+            int j = 1;
+            do
+            {
+                Console.Write("* ");
+                j++;
+            }
+            while (j <= i);
+
+            Console.WriteLine();
+            i++;
+        }
+    }
+}
+
