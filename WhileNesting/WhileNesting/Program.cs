de6e00ed743df96b2b9d063fe8330887c3901426
Opilane TTHK <opilane@tthk.ee>
2025-11-27 09:31:13 +0200

    WheileNesting

---
+++ WhileNesting/WhileNesting/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace WhileNesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //tuleb kasutada while ja omakorda while while sisse panna
+            //while nesting
+            //while kordab ennast 5 korda
+
+            Console.WriteLine("While nesting");
+
+            int a = 1;
+            //kasutada while while sees
+
+            while (a <= 5)
+            {
+                int b = 1;
+                while (b <= 5)
+                {
+                    Console.Write(b + " ");
+                    b++;
+                }
+                Console.WriteLine();
+                a++;
+            }
+            Console.ReadKey(); 
+        }
+
+    }
+}
+
