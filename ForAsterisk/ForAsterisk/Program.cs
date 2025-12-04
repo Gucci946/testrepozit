6ca63d70d61f26b81090d3db70a4d2b682f79a7e
Opilane TTHK <opilane@tthk.ee>
2025-12-04 09:37:42 +0200

    ForLoop
    ForLoopEndLess

---
+++ ForAsterisk/ForAsterisk/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ForAsterisk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For Asterisk e *");
+
+            int nr = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < nr; i++)
+            {
+                Console.Write("*");
+            }
+        }
+    }
+}
+
+
---
+++ ForLoopEndLess/ForLoopEndLess/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ForLoopEndLess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For lopp endless");
+
+            //kasutame for loopi
+            ushort i = 0;
+
+            // kui on 10 rida konsoolis, siis lõpetab ära
+            //VIHJE kasutage break-i
+
+            for (; ; )
+
+            {
+                Console.WriteLine("Rida: " + (i + 1));
+                i++;
+
+                if (i == 10)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("Tsükkel lõpetatud!");
+        }
+    }
+}
