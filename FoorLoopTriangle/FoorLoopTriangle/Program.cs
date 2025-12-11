b6432ee4f6dca9f69dd876cbc5b019747186b053
Opilane TTHK <opilane@tthk.ee>
2025-12-11 10:55:14 +0200

    FoorLoopTriangle

---
+++ FoorLoopTriangle/FoorLoopTriangle/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace FoorLoopTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+            {
+                Console.WriteLine("Tärnidest tehtud kolmnurke poolik püramiid:");
+                Console.WriteLine("============================================");
+                Console.Write("Sisesta ridade arv: ");
+
+                int rows = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                for (int i = 1; i <= rows; i++)
+                {
+                    for (int j = 1; j <= i; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine("\nVajuta suvalist klahvi, et sulgeda...");
+                Console.ReadKey();
+            }
+            }
+
+        }
+    }
+}
