e24bfdaa59fa3794df75af163b90c219b5bab326
Opilane TTHK <opilane@tthk.ee>
2025-12-11 11:08:35 +0200

    FoorLoopPyramid

---
+++ FoorLoopPyramid/FoorLoopPyramid/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FoorLoopPyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, n;
+
+            Console.WriteLine("Loo püramid tärnidest:\n");
+            Console.WriteLine("--------------------\n");
+            Console.WriteLine("Sisesta püramiidi suurus:");
+
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nPüramiid:\n");
+
+            for (i = 1; i <= n; i++)
+            {               
+                for (j = 1; j <= n - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                
+                for (j = 1; j <= 2 * i - 1; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
